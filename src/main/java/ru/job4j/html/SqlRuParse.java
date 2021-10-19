6d3426edc9b0e2ae98193308b7b74be0289898cd
MishailEx <Mishail.avram@mail.ru>
2021-10-19 20:21:14 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,15 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
-        for (Element td: row) {
+        for (Element td : row) {
             Element parent = td.parent();
-            System.out.println(parent.child(5).text());
+            SqlRuDateTimeParser dt = new SqlRuDateTimeParser();
+            System.out.println(dt.parse(parent.child(5).text()));
         }
-
     }
 }
