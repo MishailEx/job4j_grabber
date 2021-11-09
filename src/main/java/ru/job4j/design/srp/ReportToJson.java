5a6db86f081efa041f80d285c7cdcf1c5c9f8501
MishailEx <Mishail.avram@mail.ru>
2021-11-09 18:37:21 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #225074]

--- src/main/java/ru/job4j/design/srp/ReportToJson.java
+++ src/main/java/ru/job4j/design/srp/ReportToJson.java
@@ -3,9 +3,6 @@ package ru.job4j.design.srp;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class ReportToJson implements Report {
@@ -21,22 +18,5 @@ public class ReportToJson implements Report {
         Gson gson = new GsonBuilder().create();
         text.append(gson.toJson(store.findBy(filter)));
         return text.toString();
-    }
-
-    public static void main(String[] args) {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee e = new Employee("dd", now, now, 200);
-        store.add(e);
-        List<Employee> list = new ArrayList<Employee>();
-        list.add(e);
-//        ReportToJson report = new ReportToJson(store);
-//        String s = report.generate(em -> true);
-//        System.out.println(s);
-        Gson gson = new GsonBuilder().create();
-        String sd = "sddfsdf, sdfdsf, sfdf";
-        System.out.println(gson.toJson(list));
-
-
     }
 }
